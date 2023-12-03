745bc2da0b2eb53b1596236e1c43a90fd67f4f16
hongbowu <wuhongbo87@gmail.com>
2023-12-02 21:46:51 -0500

    add current time

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+let saveBtn = $(".btn");
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -8,6 +9,11 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+  saveBtn.on("click", function (event) {
+    // let chosen = event.target;
+    event.preventDefault();
+  });
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -20,4 +26,9 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  let currentTime = dayjs();
+  function timeRuns() {
+    $(".lead").text(currentTime.format("dddd, MMMM D, YYYY h:mm A"));
+  }
+  setInterval(timeRuns, 1000);
 });
